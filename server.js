6b693085460082d99129f856169d0e8180820ab1
mtking78 <mtking78@gmail.com>
2018-05-24 15:37:16 -0700

    First files created. Dependencies installed.

---
+++ server.js
@@ -0,0 +1,44 @@
+// *** Dependencies *** //
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var request = require("request");
+
+// *** Scraping Tools *** //
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/mongo-scraper");
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+
+
+// Start the Server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
